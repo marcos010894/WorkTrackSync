c2e9a02c97d14b0f5daa91d0d920fdc066c7b3b0
marcos paulo machado azevedo <marcospaulomachadoazevedo@MacBook-Air-de-marcos.local>
2025-08-31 21:23:41 -0300

    url add

--- simple-monitor-online/agent/monitor_online.py
+++ simple-monitor-online/agent/monitor_online.py
@@ -16,7 +16,7 @@ from datetime import datetime, date
 import platform
 
 class OnlineActivityMonitor:
-    def __init__(self, server_url="https://worktracksync.com"):
+    def __init__(self, server_url="https://simple-monitor-online-ct1i7hziv-marcos10895s-projects.vercel.app"):
         # Remover apenas /api no final se presente
         if server_url.endswith('/api'):
             self.server_url = server_url[:-4]
@@ -382,7 +382,7 @@ class OnlineActivityMonitor:
 
 def main():
     # URL do servidor
-    server_url = "https://worktracksync.com"
+    server_url = "https://simple-monitor-online-ct1i7hziv-marcos10895s-projects.vercel.app"
     
     # Verificar argumentos da linha de comando
     if len(sys.argv) > 1:
